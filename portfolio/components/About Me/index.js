c3d54c9e7f6b1dd7e567f2a0ca845b4beffd65c8
lukechaplin <lukechaplin@gmail.com>
2022-02-10 14:58:48 +0000

    added about me page

---
+++ portfolio/components/About Me/index.js
@@ -0,0 +1,11 @@
+export default function AboutMe() {
+  return (
+    <p>
+      Hi, I'm Luke and as it says in the tab I am an aspiring web developer,
+      welcome to my homepage
+      <br></br>
+      At the bottom of this page you will find links to my socials and also my
+      CV and a contact form to reach out to me directly.
+    </p>
+  );
+}
