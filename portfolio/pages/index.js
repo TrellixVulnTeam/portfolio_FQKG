7a23151a8c3cf7e909d7ec94419fcd65b24e604e
lukechaplin <lukechaplin@gmail.com>
2022-02-10 15:03:40 +0000

    added about me to main index.js

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import AboutMe from "../components/About Me";
 
 export default function Home() {
   return (
     <div className="text-3xl font-bold underline">
       <p>hello world!</p>
+      <AboutMe />
     </div>
   );
 }
