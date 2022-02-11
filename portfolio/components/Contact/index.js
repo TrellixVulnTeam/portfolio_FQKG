ac0b5bacc18dc8cefd93f1191a2a3ff6861ca598
lukechaplin <lukechaplin@gmail.com>
2022-02-11 19:04:19 +0000

    contact form update

--- portfolio/components/Contact/index.js
+++ portfolio/components/Contact/index.js
@@ -3,34 +3,18 @@ export default function ContactMe() {
     <div>
       <form action="https://usebasin.com/f/4dbdd7706b10" method="POST">
         <label>First Name</label>
-        <input
-          type="text"
-          id="fname"
-          name="firstname"
-          placeholder="Your first name.."
-        />
+        <input type="text" name="firstname" placeholder="Your first name.." />
 
         <label>Last Name</label>
-        <input
-          type="text"
-          id="lname"
-          name="lastname"
-          placeholder="Your last name.."
-        />
+        <input type="text" name="lastname" placeholder="Your last name.." />
 
         <label>E-mail</label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          placeholder="Enter your e-mail"
-        />
+        <input type="text" name="email" placeholder="Enter your e-mail" />
 
         <label>Subject</label>
         <textarea
-          id="subject"
           name="subject"
-          placeholder="Write something.."
+          placeholder="Write your message here"
         ></textarea>
 
         <input name="button" type="submit" value="Submit"></input>
