be4117f58e212aee81b725ca47b9726a7c540086
lukechaplin <lukechaplin@gmail.com>
2022-02-23 23:10:40 +0000

    social icons aligned

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "275e6030687260a5"; }
+/******/ 		__webpack_require__.h = function() { return "34c07dd5b4dd9cdc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -11,43 +11,44 @@ import {
 export default function Footer() {
   return (
     <footer className="text-2xl font-mono p-15">
-      <div class="grid justify-items-center ">
-        <a
-          class="hover:scale-150"
-          href="https://twitter.com/luke_chap"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faTwitterSquare} />
-        </a>
-        <a
-          class="hover:scale-150"
-          href="https://uk.linkedin.com/in/luke-chaplin-70a521b0?trk=people-guest_people_search-card"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a
-          class="hover:scale-150"
-          href="https://www.instagram.com/lukechap47/"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faInstagramSquare} />
-        </a>
-        <a
-          class="hover:scale-150"
-          href="https://docs.google.com/document/d/1yL9qfQDGfGMZzAIRZsP7-gSKzyRbbkNZ/edit"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faGoogleDrive} />
-        </a>
-        <a
-          class="hover:scale-150"
-          href="https://github.com/lukechaplin"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-
+      <div class="border-t flex flex-col items-center pt-6'">
+        <div className="flex mb-4 space-x-2">
+          <a
+            class="hover:scale-150"
+            href="https://twitter.com/luke_chap"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faTwitterSquare} />
+          </a>
+          <a
+            class="hover:scale-150"
+            href="https://uk.linkedin.com/in/luke-chaplin-70a521b0?trk=people-guest_people_search-card"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            class="hover:scale-150"
+            href="https://www.instagram.com/lukechap47/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faInstagramSquare} />
+          </a>
+          <a
+            class="hover:scale-150"
+            href="https://docs.google.com/document/d/1yL9qfQDGfGMZzAIRZsP7-gSKzyRbbkNZ/edit"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGoogleDrive} />
+          </a>
+          <a
+            class="hover:scale-150"
+            href="https://github.com/lukechaplin"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
         <div className="flex mb-4 space-x-4"></div>
         <div className="flex mt-2 mb-2 space-x-2">
           <div>{`© Copyright ${new Date().getFullYear()}`}</div>
